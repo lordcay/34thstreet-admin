0f739ff7ee812c776d67d8c4027e7fc51d564062
Caleb <obodehchinedu@gmail.com>
2026-02-20 14:55:17 +0100

    Add production environment variable support2

--- src/api/client.js
+++ src/api/client.js
@@ -28,15 +28,6 @@ api.interceptors.request.use((config) => {
 })
 
 
-// Attach token to every request automatically
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('adminToken')
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`
-  }
-
-  return config
-})
 
 export default api
